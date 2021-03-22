5781be374b44ffcb36244df2b021131fe26e4453
Eszter Laura Jónás <eszter.jonaska@gmail.com>
2021-03-22 10:01:29 +0100

    MapComponent fix

--- components/ListScreen/index.js
+++ components/ListScreen/index.js
@@ -1,6 +1,6 @@
 import { ScrollView, View, Image, Text } from "react-native";
-import React, { Component, useEffect } from 'react'
-import { connect, useDispatch } from 'react-redux'
+import React, { Component, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 
 // This component is responsible for displaying the previously saved runnings
 const ListScreen = () => {
