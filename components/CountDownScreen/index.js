2c87989f6823a4c3df6067a39e089d4db4571c2f
Eszter Laura Jónás <eszter.jonaska@gmail.com>
2021-05-02 15:53:35 +0200

    Add comment to CountDownScreen

--- components/CountDownScreen/index.js
+++ components/CountDownScreen/index.js
@@ -1,33 +1,30 @@
 import React from "react";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
-import {
-    View,
-    StyleSheet,
-    Animated,
-  } from "react-native";
+import { View, StyleSheet, Animated } from "react-native";
 
-
+// This component is the Counter component. On the screen a CountdownCircle will be visible for the user.
+// navigation  -- navigation
 const CountDownScreen = ({navigation}) => {
-    return(
-        <View style={styles.countDownContainer}>
-        <CountdownCircleTimer
-          isPlaying
-          duration={2}
-          onComplete={() => navigation.navigate("Action")}
-          colors={[
-            ["#004777", 0.4],
-            ["#F7B801", 0.4],
-            ["#A30000", 0.2],
-          ]}
-        >
-          {({ remainingTime, animatedColor }) => (
-            <Animated.Text style={{ color: animatedColor }}>
-              {remainingTime}
-            </Animated.Text>
-          )}
-        </CountdownCircleTimer>
-      </View>
-    )
+  return(
+      <View style={styles.countDownContainer}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={2}
+        onComplete={() => navigation.navigate("Action")}
+        colors={[
+          ["#004777", 0.4],
+          ["#F7B801", 0.4],
+          ["#A30000", 0.2],
+        ]}
+      >
+        {({ remainingTime, animatedColor }) => (
+          <Animated.Text style={{ color: animatedColor }}>
+            {remainingTime}
+          </Animated.Text>
+        )}
+      </CountdownCircleTimer>
+    </View>
+  )
 }
 
 
